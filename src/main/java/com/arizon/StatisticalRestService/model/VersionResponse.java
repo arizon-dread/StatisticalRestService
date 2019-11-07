ee51ee861a81e58362f5da61b3a9c7050f30e974
arizon-dread <arizon.es@gmail.com>
2019-11-07 21:59:00 +0100

    fixed some postgres stacktraces. project.version is still not working, neither is getAllEntities.

--- src/main/java/com/arizon/StatisticalRestService/model/VersionResponse.java
+++ src/main/java/com/arizon/StatisticalRestService/model/VersionResponse.java
@@ -3,22 +3,22 @@ package com.arizon.StatisticalRestService.model;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.util.Properties;
+import org.springframework.beans.factory.annotation.Value;
 
 public class VersionResponse {
-    String version = "";
+    //@Value("project.version")
+    @Value("${info.app.version:unknown}")
+    String version;
     private Logger log = LoggerFactory.getLogger(this.getClass());
     public VersionResponse() {
 
-        final Properties properties = new Properties();
-        try {
-            properties.load(this.getClass().getClassLoader().getResourceAsStream("application.yml"));
-            version = properties.getProperty("project.version");
-        } catch (IOException ioe) {
-            log.error(ioe.getMessage());
-        }
+//        final Properties properties = new Properties();
+//        try {
+//            properties.load(this.getClass().getClassLoader().getResourceAsStream("application.yml"));
+//            version = properties.getProperty("project.version");
+//        } catch (IOException ioe) {
+//            log.error(ioe.getMessage());
+//        }
     }
 
     public String getVersion() {
